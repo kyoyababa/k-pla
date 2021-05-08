7307bacc3c1ca5c77979f38f51fdf507375eec9a
kyoyababa <kyouya.baba@bizreach.co.jp>
2021-05-08 10:12:57 +0900

    first commit

---
+++ workspace/assets/scripts/kPla__content.ts
@@ -0,0 +1,9 @@
+import $ from 'jquery';
+import * as Utils from './utils';
+import * as i18n from './i18n';
+
+class KPla {
+  constructor() {}
+}
+
+new KPla();
---
+++ workspace/assets/scripts/utils.ts
@@ -0,0 +1,8 @@
+import $ from 'jquery';
+
+export function getAdditionalStyles(): string {
+  return `
+    <style>
+    </style>
+  `;
+}
